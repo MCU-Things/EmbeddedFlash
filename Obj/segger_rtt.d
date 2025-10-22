5b543788587c9dffcc489915a11024bb7a0fd85e
lyk <1154785854@qq.com>
2025-10-22 10:21:32 +0800

    chore: remove Obj/ directory from version control and optimize .gitignore

--- Obj/segger_rtt.d
+++
@@ -1,7 +0,0 @@
-.\obj\segger_rtt.o: APP\SEGGER\SEGGER_RTT.c
-.\obj\segger_rtt.o: APP\SEGGER\SEGGER_RTT.h
-.\obj\segger_rtt.o: .\APP\SEGGER\Config\SEGGER_RTT_Conf.h
-.\obj\segger_rtt.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\obj\segger_rtt.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
-.\obj\segger_rtt.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\obj\segger_rtt.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
--- Obj/segger_rtt_printf.d
+++
@@ -1,6 +0,0 @@
-.\obj\segger_rtt_printf.o: APP\SEGGER\SEGGER_RTT_printf.c
-.\obj\segger_rtt_printf.o: APP\SEGGER\SEGGER_RTT.h
-.\obj\segger_rtt_printf.o: .\APP\SEGGER\Config\SEGGER_RTT_Conf.h
-.\obj\segger_rtt_printf.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
-.\obj\segger_rtt_printf.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdarg.h
-.\obj\segger_rtt_printf.o: d:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
